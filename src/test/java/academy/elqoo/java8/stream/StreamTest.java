f5b3705cc49efd71b29f200ccbe31199e122a027
Erko Bridee <erko.bridee@gmail.com>
2018-05-09 19:26:54 -0300

    updates shouldGenerate10RandomNumbers test method

--- src/test/java/academy/elqoo/java8/stream/StreamTest.java
+++ src/test/java/academy/elqoo/java8/stream/StreamTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.junit.Assert.*;
 
 public class StreamTest {
+    // hello
 
     @Test
     public void shouldMapStringsToUpperCase() {
